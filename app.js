350556dda575d017ccc80766f275187ee550028e
Zdenek Podany <zdepodany@gmail.com>
2023-10-13 15:19:04 +0200

    Add reCAPTCHA, change email to google

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Force HTTPS
-app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(`https://${req.headers.host}${req.url}`);
-    } else {
-        next();
-    }
-});
+//app.use((req, res, next) => {
+//    if (req.headers['x-forwarded-proto'] !== 'https') {
+//        res.redirect(`https://${req.headers.host}${req.url}`);
+//    } else {
+//        next();
+//    }
+//});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
